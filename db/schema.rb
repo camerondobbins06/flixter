5e334a9b65da18ec0dcba4e31696c8e031b40781
cameron <camerondobbins06@gmail.com>
2016-06-06 01:09:01 +0000

    current status

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604170335) do
+ActiveRecord::Schema.define(version: 20160606002937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "courses", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.decimal  "cost"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
+
+  add_index "courses", ["user_id"], name: "index_courses_on_user_id", using: :btree
 
   create_table "lessons", force: true do |t|
     t.string   "title"
@@ -54,8 +61,5 @@ ActiveRecord::Schema.define(version: 20160604170335) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
