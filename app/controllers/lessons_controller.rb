bf78d99dcc00bb5f68e6d78c79c42b6fa696b5b2
cameron <camerondobbins06@gmail.com>
2016-06-11 04:55:07 +0000

    Verfiy if user is registered for th ecurrent course and if not then they will recieve an error messages and redirected to the course page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,26 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_video, except: :new
+
   def show
+  
   end
 
   private
+  
   helper_method :current_lesson
+  
+  def verify_course_enrollment
+  	current_user.enrolled_in?(current_course)
+  	if !current_user.enrolled_in?(curent_course)
+  		redirect_to current_course, alert: 'Please Enroll In The Course'
+  end
+
+  
+  def current_course
+  	current_lesson.section.course
+  end
+
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
