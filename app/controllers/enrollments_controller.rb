03a39a3693eaee57cab4c96ae058187bdb3447f4
cameron <camerondobbins06@gmail.com>
2016-06-11 01:32:47 +0000

    update

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,5 +1,5 @@
 class EnrollmentsController < ApplicationController
-	before_action :athenticate_user!
+	before_action :authenticate_user!
 
 	def create
 		current_user.enrollments.create(course: current_course)
--- db/migrate/20160606025329_add_video_to_courses.rb
+++
@@ -1,5 +0,0 @@
-class AddVideoToCourses < ActiveRecord::Migration
-  def change
-    add_column :lessons, :video, :string
-  end
-end
