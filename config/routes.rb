03162eb1e715bc839beb4c2631e0f137624eb5cd
cameron <camerondobbins06@gmail.com>
2016-06-12 19:57:45 +0000

    Added the ability to reorder sections

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 Flixter::Application.routes.draw do
   devise_for :users
+  resource :dashboard, only: [:show]
   root 'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :lessons, only: [:update]
-    resources :sections, only: [] do
-      resources :lessons, only: [:new, :create]
+    resources :sections, only: [:update] do
+      resources :lessons, only: [:create]
     end
     resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create]
+      resources :sections, only: [:create]
     end
   end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
